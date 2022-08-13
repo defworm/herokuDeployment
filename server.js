090f94921235ee654a5ed4653062193070566eec
Crystal Korth <crystalkorth@gmail.com>
2022-08-13 14:40:39 -0500

    fix port

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express ()
+require('dotenv').config()
+
 
 app.get ('/', (req, res) => {
 res.send ('hello world')
 })
 
 
-app.listen(3000)
+app.listen(process.env.PORT)
